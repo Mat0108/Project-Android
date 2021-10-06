33b1f389d5ff76ac255e7e0eb1a58eb45e265b64
VincentVajou <91198607+VincentVajou@users.noreply.github.com>
2021-10-06 13:55:00 +0200

    log

--- ProjectAndroid/app/src/main/java/com/example/projectandroid/MainActivity.java
+++ ProjectAndroid/app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.projectandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.connection);
+
+        Log.e("Working Buddies","test de log");// cest jsute pour tester la bonne connection
     }
 }
