c443c9300a691994df2c09d822b69f96bd173704
Mat0108 <mesjeux0108@gmail.com>
2021-10-10 22:19:14 +0200

    update recherche.xml

--- ProjectAndroid/app/src/main/java/com/example/projectandroid/MainActivity.java
+++ ProjectAndroid/app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recherche);
-
 
     }
 }
